9182c9685a18622fb467cfdcbcfda3412469a11f
rahulsingh <rahul.singh@anant.us>
2015-04-22 17:13:09 -0400

    continuing to add things to repositories

--- GA.Data/Repositories/CollectionItemRepository.cs
+++ GA.Data/Repositories/CollectionItemRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using NLog;
+using System.Collections.Generic;
+using MongoDB.Driver;
 
 namespace GA.Data
 {
@@ -8,29 +10,71 @@ namespace GA.Data
 	/// </summary>
 	public class CollectionItemRepository
 	{
+		/// <summary>
+		/// Gets the log.
+		/// </summary>
+		/// <value>The log.</value>
 		public Logger Log {
 			get;
 			private set;
 		}
 
-		public string Connection {
+		/// <summary>
+		/// Gets the connection.
+		/// </summary>
+		/// <value>The connection.</value>
+		public string ConnectionString {
 			get;
 			private set;
 		}
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="GA.Data.CollectionItemRepository"/> class.
+		/// Gets or sets the client.
 		/// </summary>
-		/// <param name="ConnectionString">Connection string.</param>
-		/// <param name="logger">Logger.</param>
-		public CollectionItemRepository (string ConnectionString, Logger logger)
-		{ 
-			this.Log = logger;
-			this.Connection = ConnectionString;
+		/// <value>The client.</value>
+		private MongoClient Client {
+			get;
+			set;
 		}
 
-		//TODO: implement method to get batch repository items 
+		/// <summary>
+		/// Gets the name of the database.
+		/// </summary>
+		/// <value>The name of the database.</value>
+		public string DatabaseName {
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GA.Data.CollectionItemRepository"/> class.
+		/// </summary>
+		/// <param name="incomingConnectionString">Incoming connection string.</param>
+		/// <param name="logger">Logger.</param>
+		public CollectionItemRepository (string incomingConnectionString, Logger logger)
+		{ 
+			this.Log = logger;
+			this.ConnectionString = incomingConnectionString;
+			this.DatabaseName = "test"; //default database name 
+			this.Client = new MongoClient (incomingConnectionString);
+
+		}
+
+
+		/// <summary>
+		/// Gets the items.
+		/// </summary>
+		/// <returns>The items.</returns>
+		public List<CollectionItem> GetItems(){
+			//TODO: implement method to get batch repository items 
 			// gets all the repository items in the repo 
+
+			Log.Info ("Retreiving Collection Items .. ");
+			var Database = Client.GetDatabase (this.DatabaseName);
+			var Collection = Database.GetCollection<CollectionItem> ("CollectionItems");
+			return (List<CollectionItem>)Collection;
+		}
 
 		//TODO: implement method to insert batch repository items 
 			// inserts a bunch of items at the same time 
---
+++ GA.Data/Repositories/WebPageRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using System.Net;
+using NLog;
+
+namespace GA.Data
+{
+	public class WebPageRepository
+	{
+		/// <summary>
+		/// Gets the log.
+		/// </summary>
+		/// <value>The log.</value>
+		public Logger Log {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets the connection.
+		/// </summary>
+		/// <value>The connection.</value>
+		public string ConnectionURL {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets or sets the client.
+		/// </summary>
+		/// <value>The client.</value>
+		private WebClient Client {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GA.Data.WebPageRepository"/> class.
+		/// </summary>
+		/// <param name="incomingConnectionURL">Incoming connection UR.</param>
+		/// <param name="logger">Logger.</param>
+		public WebPageRepository (string incomingConnectionURL, Logger logger)
+		{
+			this.Log = logger;
+			this.ConnectionURL = incomingConnectionURL;
+
+		}
+
+		//TODO: implement a page scraper via NReadability or an API 
+		// maybe use a REST service to get image/ etc and NReadability to the article itself
+		// https://www.mashape.com/pbkwee/html2text
+	}
+}
+
