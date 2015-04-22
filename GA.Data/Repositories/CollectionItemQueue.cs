df7a7bba06c738448664161fd1f54ceaf07c8062
rahulsingh <rahul.singh@anant.us>
2015-04-21 22:56:59 -0400

    setting up the Queue and Item  repository

---
+++ GA.Data/Repositories/CollectionItemQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NLog;
+
+namespace GA.Data
+{
+	/// <summary>
+	/// Collection item queue. Gets / Removes items from the Collection Item queue in MySQL
+	/// </summary>
+	public class CollectionItemQueue
+	{
+		public Logger Log {
+			get;
+			private set;
+		}
+
+		public string Connection {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GA.Data.CollectionItemQueue"/> class.
+		/// </summary>
+		/// <param name="ConnectionString">Connection string.</param>
+		/// <param name="logger">Logger.</param>
+		public CollectionItemQueue (string ConnectionString, Logger logger)
+		{ 
+			this.Log = logger;
+			this.Connection = ConnectionString;
+		}
+
+		//TODO: implement method to get batch repository items 
+			// gets unprocessed items 
+
+		//TODO: implement method to insert individual repository items 
+			// adds one record - will probably do this in gridview 
+
+		//TODO: implement method to insert batch repository items 
+			// not needed - data will be added manually 
+
+		//TODO: implement method to get individual repository items
+			// not needed - we'll be batch processing
+
+
+	}
+}
+
---
+++ GA.Data/Repositories/CollectionItemRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NLog;
+
+namespace GA.Data
+{
+	/// <summary>
+	/// Collection item repository. Gets / adds information to a MongoDB Collection
+	/// </summary>
+	public class CollectionItemRepository
+	{
+		public Logger Log {
+			get;
+			private set;
+		}
+
+		public string Connection {
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GA.Data.CollectionItemRepository"/> class.
+		/// </summary>
+		/// <param name="ConnectionString">Connection string.</param>
+		/// <param name="logger">Logger.</param>
+		public CollectionItemRepository (string ConnectionString, Logger logger)
+		{ 
+			this.Log = logger;
+			this.Connection = ConnectionString;
+		}
+
+		//TODO: implement method to get batch repository items 
+			// gets all the repository items in the repo 
+
+		//TODO: implement method to insert batch repository items 
+			// inserts a bunch of items at the same time 
+
+		//TODO: implement method to get individual repository items
+			// not needed right now - will be able to get individual view 
+
+		//TODO: implement method to insert individual repository items 
+			// not needed right now - will be able to add items incrementally 
+
+	}
+}
+
